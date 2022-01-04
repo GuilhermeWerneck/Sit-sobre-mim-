26fec641d254d0a29622287a633dde031f486d78
Guilherme Werneck <onrushingmilk50@hotmail.com>
2022-01-04 12:18:48 -0300

    php

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 function abrirHTML() {
-    if (document.getElementById("curso_css").style.display = "block"  || document.getElementById("curso_js").style.display == "block" ){
+    if (document.getElementById("curso_css").style.display = "block"  || document.getElementById("curso_js").style.display == "block" || document.getElementById("curso_php").style.display == "block" ){
     document.getElementById("curso_js").style.display = "none";
+    document.getElementById("cursos_php").style.display = "none";
     document.getElementById("curso_css").style.display = "none";
     document.getElementById("curso_html").style.display = "block"
     }
 }
 function abrirCSS() {
-    if (document.getElementById("curso_html").style.display = "block"  || document.getElementById("curso_js").style.display == "block" ){ 
+    if (document.getElementById("curso_html").style.display = "block"  || document.getElementById("curso_js").style.display == "block"  || document.getElementById("curso_php").style.display == "block"){ 
+
         document.getElementById("curso_html").style.display = "none";
+        document.getElementById("curso_php").style.display = "none";
         document.getElementById("curso_js").style.display = "none";
         document.getElementById("curso_css").style.display = "block";
     } 
 }
 
 function abrirJS() {
-    if (document.getElementById("curso_html").style.display = "block" || document.getElementById("curso_css").style.display == "block" ){ 
+    if (document.getElementById("curso_html").style.display = "block" || document.getElementById("curso_css").style.display == "block" || document.getElementById("curso_php").style.display == "block" ){ 
         
         document.getElementById("curso_html").style.display = "none";
+        document.getElementById("curso_php").style.display = "none";
         document.getElementById("curso_css").style.display = "none";
         document.getElementById("curso_js").style.display = "block";
     }      
+}
+
+
+function abrirPHP(){
+    if (document.getElementById("curso_html").style.display = "block" || document.getElementById("curso_css").style.display == "block" || document.getElementById("curso_js").style.display == "block"){ 
+        document.getElementById("curso_js").style.display = "none";     
+        document.getElementById("curso_html").style.display = "none";
+        document.getElementById("curso_css").style.display = "none";
+        document.getElementById("curso_php").style.display = "block";
+    }
 }
